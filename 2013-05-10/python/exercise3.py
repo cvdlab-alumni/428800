4da37c2a49fc1cfa45643ee13dfbd90ee0823166
raff0z <razorzeta@gmail.com>
2013-05-11 17:12:54 +0200

    aggiunti dettagli alla ruota ... purtroppo bisogna sistemare il dominio per far vedere le ruote insieme

--- 2013-05-10/python/exercise3.py
+++ 2013-05-10/python/exercise3.py
@@ -1,14 +1,12 @@
-#esercizio 3
+#exercise 3
 def traslaPointsZ(points,value):
 	return map(lambda item:\
 		[item[0],item[1],item[2]+value], points)
 
-#Funzione che trasla i punti sull'asse y
 def traslaPointsY(points,value):
 	return map(lambda item:\
 		[item[0],item[1]+value,item[2]], points)
 
-#Funzione che trasla i punti sull'asse x
 def traslaPointsX(points,value):
 	return map(lambda item:\
 		[item[0]+value,item[1],item[2]], points)
@@ -48,13 +46,19 @@ def extrude(obj,z):
 
 
 gomma1=bezier_circle_map(0.5,S1)
-gomma2=bezier_circle_map(0.3,S1)
+gomma2=bezier_circle_map(0.27,S1)
 gomma3 = bezier_circle_not_centered_map(0.5,0,0,-0.25,S1)
-gomma4 = bezier_circle_not_centered_map(0.3,0,0,-0.25,S1)
+gomma4 = bezier_circle_not_centered_map(0.27,0,0,-0.26,S1)
 
 
-cgomma1 = MAP(gomma1)(INTERVALS(1)(32))
-cgomma2 = MAP(gomma2)(INTERVALS(1)(32))
+gomma4b = bezier_circle_not_centered_map(0.3,0,0,-0.26,S1)
+gomma2b = bezier_circle_not_centered_map(0.3,0,0,0.01,S1)
+
+scgomma4 = BEZIER(S2)([gomma4b,gomma4])
+scgomma2 = BEZIER(S2)([gomma2b,gomma2])
+
+cgomma4 = MAP(scgomma4)(domain2)
+cgomma2 = MAP(scgomma2)(domain2)
 
 sgomma = BEZIER(S2)([gomma1,gomma2])
 sgomma2 = BEZIER(S2)([gomma3,gomma4])
@@ -67,21 +71,17 @@ ogomma3 = MAP(sgomma3)(domain2)
 ogomma4 = MAP(sgomma4)(domain2)
 
 ruota = STRUCT([ogomma,ogomma2,ogomma3,ogomma4])
+ruota = COLOR(BLACK)(ruota)
+ruota = STRUCT([ruota,cgomma4,cgomma2])
 ruota = R([2,3])((PI/2))(ruota)
 ruota = T([1,3])([2,0.49])(ruota)
 
 
-# [[1.71, 4.08,0], [1.88, 3.97,0], [1.95, 3.98,0], [1.92, 4.2,0]]
-
 cerchione1points = [[1.71-1.71, 4.08-4.08,0], [1.88-1.71, 3.97-4.08,0], [1.95-1.71, 3.9-4.08,0], [1.92-1.71, 4.2-4.08,0]]
 fcerchione1 = BEZIER(S1)(cerchione1points)
-# ccerchione1 = MAP(fcerchione1)(domain)
-
-# [[1.71, 4.08,0], [1.75, 4.17,0], [1.83, 4.21,0], [1.92, 4.2,0]]
 
 cerchione2points = [[1.71-1.71, 4.08-4.08,0], [1.75-1.71, 4.17-4.08,0], [1.83-1.71, 4.21-4.08,0], [1.92-1.71, 4.2-4.08,0]]
 fcerchione2 = BEZIER(S1)(cerchione2points)
-# ccerchione2 = MAP(fcerchione2)(domain)
 
 ffcerchione = BEZIER(S2)([fcerchione1,fcerchione2])
 
@@ -100,10 +100,17 @@ scerchione2 = T([3])([0.25])(scerchione)
 
 cerchione = STRUCT([scerchione,scerchione2z,scerchione1z,scerchione2])
 
-
 cerchione = T([1,2])([-0.23,0.15])(cerchione)
-# cerchione = R([1,2])(-0.2)(cerchione)
-# mat = CONS(AA(R([1,2]))([(12*PI)/30,(12*PI)/30,(12*PI)/30,(12*PI)/30]))
+cerchione = COLOR(BLACK)(cerchione)
+
+bullonecerchione = SPHERE(0.02)([10,10])
+bullonecerchione2 = T([1,2,3])([-0.085,0.15,0])(bullonecerchione)
+bullonecerchione2 = COLOR(BROWN)(bullonecerchione2)
+bullonecerchione = T([1,2,3])([-0.085,0.15,0.25])(bullonecerchione)
+bullonecerchione = COLOR(BROWN)(bullonecerchione)
+
+cerchione = STRUCT([cerchione,bullonecerchione,bullonecerchione2])
+
 c1 = R([1,2])((12*PI)/30)(cerchione)
 c2 = R([1,2])((12*PI)/30)(c1)
 c3 = R([1,2])((12*PI)/30)(c2)
@@ -112,9 +119,8 @@ c5 = R([1,2])((12*PI)/30)(c4)
 
 
 
-# cerchions = STRUCT(mat(cerchione))
 cerchions2d = STRUCT([c1,c2,c3,c4,c5])
-# cerchions3d = PROD([(cerchions2d),QUOTE([1])])
+
 cerchions2d = R([2,3])((-PI/2))(cerchions2d)
 cerchions2d = T([1,3])([2,0.49])(cerchions2d)
 
@@ -123,11 +129,19 @@ cerchio = extrude(cerchio,0.05)
 cerchio = R([2,3])((-PI/2))(cerchio)
 cerchio = T([1,2,3])([2,0.075,0.49])(cerchio)
 
+centroruota = CYLINDER ([0.08,0.25])(16)
+centroruota = R([2,3])((-PI/2))(centroruota)
+centroruota = T([1,2,3])([2,0,0.49])(centroruota)
 
-wheel = STRUCT([COLOR(BLACK)(ruota),COLOR(RED)(cerchions2d),COLOR(YELLOW)(cerchio)])
+wheel = STRUCT([ruota,cerchions2d,COLOR(YELLOW)(cerchio),centroruota])
+
+
+# VIEW(wheel)
+
 wheel = T([1,2,3])([-4,-1.5,-0.4])(wheel)
 w1 = T([1])([3.95])(wheel)
 w2 = T([2])([2.70])(wheel)
 w3 = T([1,2])([3.95,2.70])(wheel)
 
 wheels4 = STRUCT([wheel,w1,w2,w3])
+VIEW(wheels4)
